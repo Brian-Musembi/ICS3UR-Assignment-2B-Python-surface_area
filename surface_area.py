89516873ed5947348d3e1c109d447bc06d9958e0
Brian Musembi <brian.musembi@mths.ca>
2021-04-29 21:29:52 +0000

    added surface_area.py

---
+++ surface_area.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# Created by: Brian Musembi
+# Created on: 29 April 2021
+# This program calculates the surface area of a square-based pyramid
+#     where the user gets to enter the base, height mm
+
+import math
+
+
+def main():
+    # main function
+
+    print("Let's calculate the surface area of a square pyramid. ")
+    print("")
+
+    # input
+    base = int(input("Enter the base length (mm): "))
+    height = int(input("Enter the height (mm): "))
+
+    # process
+    surface_area = (base * base) + (base + base) * math.sqrt(((
+        base * base) / 4) + (height * height))
+    print("")
+
+    # output
+    print("The surface area is {} mm²".format(surface_area))
+
+
+if __name__ == "__main__":
+    main()
